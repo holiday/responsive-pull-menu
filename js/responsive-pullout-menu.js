2cc650cb63b5f2ecc3ef0df899f8cf75aa0d57d9
Rashaad Ramdeen <rashaad.ramdeen@gmail.com>
2013-10-26 02:56:17 -0400

    initial commit

---
+++ js/responsive-pullout-menu.js
@@ -0,0 +1,18 @@
+$(function(){
+
+	var position = 0; //current status of side menu, 0 = closed, 1 = open
+	var speed = 150; //in ms
+
+	$('#responsive-btn').click(function(){
+		if(position == 0){
+			$('nav#left').animate({"left": '+=240px'}, speed);
+			$('#wrapper').animate({"left": '+=240px'}, speed);
+			position = 1;
+		}else{
+			$('nav#left').animate({"left": '-=240px'}, speed);
+			$('#wrapper').animate({"left": '-=240px'}, speed);
+			position = 0;
+		}
+		
+	});
+});
