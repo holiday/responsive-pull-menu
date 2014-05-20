102fa72ed02976a5c8ab1a3cf85aa423ef00071e
Rashaad Ramdeen <rashaad.ramdeen@gmail.com>
2014-05-19 22:34:13 -0400

    implemented scroll prevention, example index.html not updated

--- js/responsive-pullout-menu.js
+++ js/responsive-pullout-menu.js
@@ -7,12 +7,30 @@ $(function(){
 		if(position == 0){
 			$('nav#left').animate({"margin-left": '+=240px'}, speed);
 			$('#wrapper').animate({"margin-left": '+=240px'}, speed);
+			$('#wrapper #overlay').show();
 			position = 1;
 		}else{
 			$('nav#left').animate({"margin-left": '-=240px'}, speed);
 			$('#wrapper').animate({"margin-left": '-=240px'}, speed);
+			$('#wrapper #overlay').hide();
 			position = 0;
 		}
 		
 	});
+
+	$('html,body').on({
+		'mousewheel' : function(e) {
+			if(position) {
+				e.preventDefault();
+				e.stopPropagation();
+			}
+		},
+		'mousedown' : function(e) {
+			if(e.which == 2 && position) {
+				e.preventDefault();
+				e.stopPropagation();
+			}
+		}
+	});
+
 });
