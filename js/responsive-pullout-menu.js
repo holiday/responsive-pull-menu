1769e496ca6f52e6d4943a0e55055289acaccf0a
Rashaad Ramdeen <rashaad.ramdeen@gmail.com>
2014-05-19 22:46:13 -0400

    implemented feature to allow menu close if the overlay is clicked

--- js/responsive-pullout-menu.js
+++ js/responsive-pullout-menu.js
@@ -2,22 +2,34 @@ $(function(){
 
 	var position = 0; //current status of side menu, 0 = closed, 1 = open
 	var speed = 150; //in ms
+	var wrapper = $('#wrapper');
+	var wrapper_overlay = $('#wrapper #overlay');
+	var nav_left = $('nav#left');
 
+	function open_menu() {
+		nav_left.animate({"margin-left": '+=240px'}, speed);
+		wrapper.animate({"margin-left": '+=240px'}, speed);
+		wrapper_overlay.show();
+		position = 1;
+	}
+
+	function close_menu(){
+		nav_left.animate({"margin-left": '-=240px'}, speed);
+		wrapper.animate({"margin-left": '-=240px'}, speed);
+		wrapper_overlay.hide();
+		position = 0;
+	}
+
+	//listen for button click to expand/fold menu
 	$('#responsive-btn').click(function(){
 		if(position == 0){
-			$('nav#left').animate({"margin-left": '+=240px'}, speed);
-			$('#wrapper').animate({"margin-left": '+=240px'}, speed);
-			$('#wrapper #overlay').show();
-			position = 1;
+			open_menu();
 		}else{
-			$('nav#left').animate({"margin-left": '-=240px'}, speed);
-			$('#wrapper').animate({"margin-left": '-=240px'}, speed);
-			$('#wrapper #overlay').hide();
-			position = 0;
+			close_menu();
 		}
-		
 	});
 
+	//prevent scrolling when expanded
 	$('html,body').on({
 		'mousewheel' : function(e) {
 			if(position) {
@@ -31,6 +43,11 @@ $(function(){
 				e.stopPropagation();
 			}
 		}
+	});
+
+	//if the user clicks the overlay, we should close the menu
+	wrapper_overlay.click(function(){
+		close_menu();
 	});
 
 });
